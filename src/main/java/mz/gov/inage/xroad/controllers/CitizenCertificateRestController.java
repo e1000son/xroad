a219ba9e5699de5a3b10d50cef9bdebeeab025ef
e1000son <emilsonvontade@gmail.com>
2024-01-17 11:56:44 +0200

    Implementing REST findCitizen API

---
+++ src/main/java/mz/gov/inage/xroad/controllers/CitizenCertificateRestController.java
@@ -0,0 +1,19 @@
+package mz.gov.inage.xroad.controllers;
+
+import mz.gov.inage.xroad.entities.BirthCertificate;
+import mz.gov.inage.xroad.repos.IBirthCertificateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CitizenCertificateRestController {
+    @Autowired
+    IBirthCertificateRepository certificateRepository;
+    @RequestMapping("/citizens/{nuic}")
+    public BirthCertificate findCitizenCertificate(@PathVariable("nuic") String nuic){
+        BirthCertificate certificate = certificateRepository.findCertificateByNuic(nuic);
+        return certificate;
+    }
+}
--- src/main/java/mz/gov/inage/xroad/repos/IBirthCertificateRepository.java
+++ src/main/java/mz/gov/inage/xroad/repos/IBirthCertificateRepository.java
@@ -3,6 +3,13 @@ package mz.gov.inage.xroad.repos;
 import mz.gov.inage.xroad.entities.BirthCertificate;
 import mz.gov.inage.xroad.entities.PlaceOfBirth;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface IBirthCertificateRepository extends JpaRepository<BirthCertificate, Long> {
+
+    @Query("SELECT n from BirthCertificate n WHERE n.nuic = :reference")
+    BirthCertificate findCertificateByNuic(@Param("reference") String reference);
 }
