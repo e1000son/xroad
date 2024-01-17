d72d54cacc9612673f2cdc2290be004201732981
e1000son <emilsonvontade@gmail.com>
2024-01-17 12:09:38 +0200

    Adding Cross Site Origin support

--- src/main/java/mz/gov/inage/xroad/controllers/CitizenCertificateRestController.java
+++ src/main/java/mz/gov/inage/xroad/controllers/CitizenCertificateRestController.java
@@ -3,11 +3,13 @@ package mz.gov.inage.xroad.controllers;
 import mz.gov.inage.xroad.entities.BirthCertificate;
 import mz.gov.inage.xroad.repos.IBirthCertificateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class CitizenCertificateRestController {
     @Autowired
     IBirthCertificateRepository certificateRepository;
